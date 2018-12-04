65b281dab7cc69741b580e15975a91c0388e1bc4
bob0the0mighty <bob0the0mighty@gmail.com>
2018-12-03 22:22:03 -0500

    Code for Day2, need to sleep

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -1,10 +1,29 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 
 module Day2 =
 
+  let getCountOfNumbers(matchCount: int)(input: string) =
+     input.ToCharArray()
+    |> Seq.groupBy (fun c -> c)
+    |> Map.ofSeq
+    |> Map.map (fun k v -> Seq.length v)
+    |> Map.exists (fun _ value -> value = matchCount)
+
+  let CreateChecksum input =
+    
+    input 
+    |> Seq.map (fun line -> (getCountOfNumbers 2 line, getCountOfNumbers 3 line))
+    |> Seq.fold (fun accum item -> item) 0
+
   [<EntryPoint>]
   let main argv =
-      printfn "Hello World from F#!"
-      0 // return an integer exit code
+    let input = File.ReadLines(argv.[0])
+
+    input 
+    |> CreateChecksum
+    |> Console.WriteLine
+    
+    0 // return an integer exit code
