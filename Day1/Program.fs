96968512be226c12470925be1611703557938b84
David P <david.patterson@guidestar.org>
2018-12-05 10:11:08 -0500

    more day1 Changes

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,8 +1,44 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open System.IO
+open System.Collections.Generic
+open System.Collections.Generic
 
-open System
+module Seq =
+  let rec cycle xs = 
+    seq { yield! xs; yield! cycle xs}
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+module Day1 = 
+
+  let CalculateFrequency inputs = 
+    Seq.sumBy Int32.Parse inputs
+
+  let CalculateAndStoreFrequency(dictionary: Dictionary<int, int>, sum: int, input: string) =
+    input
+    |> Int32.Parse
+    |> Array.mapi
+
+    
+
+  let CalculateDuplicateFreq inputs =
+    let frequencies = new Dictionary<int, int>()
+    let mut sum = 0
+
+    inputs
+    |> Seq.takeWhile (fun item -> CalculateAndStoreFrequency(frequencies, sum, item))
+
+
+
+  [<EntryPoint>]
+  let day1 args =
+    let input = File.ReadLines(args.[0])
+    let values = Seq.append ["0"] input
+    
+    values
+    |> CalculateFrequency
+    |> Console.WriteLine
+
+    values
+    |> Seq.cycle
+    |> CalculateDuplicateFreq
+
+    1
