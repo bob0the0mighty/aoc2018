cb10dbff7827a32636e3ea4baababb5746178ac8
David P <david.patterson@guidestar.org>
2018-12-07 14:55:40 -0500

    Progress on Day2

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -5,25 +5,40 @@ open System.IO
 
 module Day2 =
 
-  let getCountOfNumbers(matchCount: int)(input: string) =
+  let charactersAppearTimes(matchCount: int)(input: string) =
      input.ToCharArray()
     |> Seq.groupBy (fun c -> c)
     |> Map.ofSeq
     |> Map.map (fun k v -> Seq.length v)
     |> Map.exists (fun _ value -> value = matchCount)
 
+  let countAppearances state input =
+    let firstCount = 
+      if fst input then (fst state) + 1 
+      else (fst state)
+
+    let secoundCount = 
+      if snd input then (snd state) + 1 
+      else (snd state)
+
+    (firstCount, secoundCount)
+
   let CreateChecksum input =
-    
     input 
-    |> Seq.map (fun line -> (getCountOfNumbers 2 line, getCountOfNumbers 3 line))
-    |> Seq.fold (fun accum item -> item) 0
+    |> Seq.map (fun line -> (charactersAppearTimes 2 line, charactersAppearTimes 3 line))
+    |> Seq.fold countAppearances (0, 0)
 
   [<EntryPoint>]
   let main argv =
     let input = File.ReadLines(argv.[0])
 
+    //Day 2 Part 1
     input 
     |> CreateChecksum
-    |> Console.WriteLine
+    |> fun (x,y) -> x * y
+    |> printf "%i"
+
+    //Day 2 Part 2
+    
     
     0 // return an integer exit code
