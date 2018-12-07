bc42e856c3319f9cc096fef230b58bffcc713bf7
David P <david.patterson@guidestar.org>
2018-12-07 10:48:51 -0500

    Finished day one, learned a bit more about F# and how to properly do things

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,43 +1,32 @@
 ﻿open System
 open System.IO
-open System.Collections.Generic
 
 module Seq =
-  let rec cycle xs = 
-    seq { yield! xs; yield! cycle xs}
+  let repeat items = seq { while true do yield! items }
 
 module Day1 = 
 
   let CalculateFrequency inputs = 
     Seq.sumBy Int32.Parse inputs
 
-  let CalculateAndStoreFrequency(dictionary: Dictionary<int, int>, sum: int, input: string) =
-    input
-    |> Int32.Parse
-    |> Array.mapi
-
-    
-
-  let CalculateDuplicateFreq inputs =
-    let frequencies = new Dictionary<int, int>()
-    let mut sum = 0
-
-    inputs
-    |> Seq.takeWhile (fun item -> CalculateAndStoreFrequency(frequencies, sum, item))
-
-
-
   [<EntryPoint>]
   let day1 args =
     let input = File.ReadLines(args.[0])
-    let values = Seq.append ["0"] input
     
-    values
+    //Day 1 Part 1
+    input
     |> CalculateFrequency
     |> Console.WriteLine
 
-    values
-    |> Seq.cycle
-    |> CalculateDuplicateFreq
+    //Day 1 Part 2
+    //Coppied from https://github.com/andreasjhkarlsson/aoc-2018/blob/master/src/Day1.fs
+    //I realize how use to imperitive structures I am when reading other peoples code.
+    input
+    |> Seq.map Int32.Parse
+    |> Seq.repeat
+    |> Seq.scan (fun (f, seen) d ->  f + d, seen |> Set.add f) (0, Set.empty) // Calculate intermediate frequency sums
+    |> Seq.find (fun (f, seen) -> seen |> Set.contains f) // Find first frequency sum that is already in seen set
+    |> fst
+    |> Console.WriteLine
 
     1
