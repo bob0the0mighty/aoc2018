4c47ff574d137f83b0da56ca634f54a384742131
bob0the0mighty <bob0the0mighty@gmail.com>
2018-12-10 22:34:27 -0500

    Day 3 merge in

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,27 +1,58 @@
 ﻿open System
 open System.IO
+open System.Collections.Generic
+open System.Collections.Generic
 
 module Day3 =
 
-  let caclulateAreas(line:string) = 
+  let caclulatePoints(line:string) = 
     let splits = line.Split(" ")
     
-    let start = (splits.[2].Replace(":","").Split(","))
-    |> Array.map (fun item -> Int32.Parse(item))
+    let claim = splits.[0]
 
-    let area = splits.[3].Split("x")
-    |> Array.map Int32.Parse
+    let startPoint = 
+      splits.[2].Replace(":","").Split(",")
+      |> Array.map (fun item -> Int32.Parse(item))
 
-    seq { for newX in x .. (x + width) do
-          for newY in y .. (y + height) do
-            yield (newX, newY) }
+    let x, y = startPoint.[0], startPoint.[1]
+
+    let area = 
+      splits.[3].Split("x")
+      |> Array.map (fun item -> Int32.Parse(item))
+
+    let width, height = area.[0], area.[1]
+
+    seq { for newX in x .. (x + width - 1) do
+          for newY in y .. (y + height - 1) do
+            yield (claim, newX, newY) }
 
 
   [<EntryPoint>]
   let main argv =
     let input = File.ReadAllLines(argv.[0])
-    
-    input 
-    |> Seq.map caclulateAreas
+
+    let pointsSeq = Seq.map caclulatePoints input
+    Seq.toArray pointsSeq |> printf "%A"
+
+    let points = Seq.concat pointsSeq
+    Seq.toArray points |> printf "%A"
+
+    let pointCount = Seq.countBy (fun (a, x, y) -> (x, y) ) points
+    Seq.toArray pointCount |> printf "%A"
+
+    let distinctPoint = Seq.distinctBy (fun (a, x, y) -> (x, y) ) points
+    printf "Distinct"
+    Seq.toArray distinctPoint |> printf "%A"
+
+    let overlap = Seq.filter (fun tup-> snd tup > 1) pointCount 
+    Seq.toArray overlap |> printf "%A"
+
+    // input
+    // |> Seq.map caclulatePoints
+    // |> Seq.concat
+    // |> Seq.countBy (fun (a, x, y) -> (x, y) )
+    // |> Seq.filter (fun tup-> snd tup > 1)
+    // |> Seq.length
+    // |> printf "%i"
 
     0
