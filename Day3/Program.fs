1da749c6fd8359630f8ffb1d557ef02d679fb0ba
bob0the0mighty <bob0the0mighty@gmail.com>
2018-12-12 20:41:28 -0500

    eh, I've found a very inefficient way to solve the problem

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -1,7 +1,6 @@
 ﻿open System
 open System.IO
 open System.Collections.Generic
-open System.Collections.Generic
 
 module Day3 =
 
@@ -24,7 +23,7 @@ module Day3 =
 
     seq { for newX in x .. (x + width - 1) do
           for newY in y .. (y + height - 1) do
-            yield (claim, newX, newY) }
+            yield (claim, (newX, newY)) }
 
 
   [<EntryPoint>]
@@ -32,20 +31,28 @@ module Day3 =
     let input = File.ReadAllLines(argv.[0])
 
     let pointsSeq = Seq.map caclulatePoints input
-    Seq.toArray pointsSeq |> printf "%A"
+    //Seq.toArray pointsSeq |> printf "%A"
 
     let points = Seq.concat pointsSeq
-    Seq.toArray points |> printf "%A"
+    //Seq.toArray points |> printf "%A"
 
-    let pointCount = Seq.countBy (fun (a, x, y) -> (x, y) ) points
-    Seq.toArray pointCount |> printf "%A"
-
-    let distinctPoint = Seq.distinctBy (fun (a, x, y) -> (x, y) ) points
-    printf "Distinct"
-    Seq.toArray distinctPoint |> printf "%A"
+    let pointCount = Seq.countBy (fun (a, (x, y)) -> (x, y) ) points
+    //Seq.toArray pointCount |> printf "%A"
 
     let overlap = Seq.filter (fun tup-> snd tup > 1) pointCount 
     Seq.toArray overlap |> printf "%A"
+
+    let overlapSansCount = 
+      overlap
+      |> Seq.map (fun (point, _) -> point)
+
+    let pointCount2 = 
+      points
+      |> Seq.groupBy (fun (a, _) -> a )
+      |> Seq.map (fun (claim, item) -> (claim, Seq.map (fun (_, point) -> point) item) )
+      |> Seq.filter (fun (_, points) ->  not (Seq.exists (fun point -> Seq.contains point points) overlapSansCount ) )
+    
+    Seq.toArray pointCount2 |> printf "%A"
 
     // input
     // |> Seq.map caclulatePoints
